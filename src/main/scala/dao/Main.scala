1a0b58eed36a688f9ff430024c96230fd11073d9
Tanmoy248 <tanmoy.official@gmail.com>
2020-05-19 03:47:50 +0400

    update name

--- src/main/scala/dao/Main.scala
+++ src/main/scala/dao/Main.scala
@@ -4,7 +4,7 @@ import play.api.libs.json.JsValue
 
 object Main extends App{
   println("Starting the App...")
-  val topics = List("lalamove-test-1")
+  val topics = List("la-test-1")
   val groupId = "test-2"
   val bootstrapServers = "kafka-python:9092"
   val stream = StreamDao.start(topics,groupId, bootstrapServers)
